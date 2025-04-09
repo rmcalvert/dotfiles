ce3ba031385f45f31fbf68ecc0063176c49641db
Ryan Calvert <ryan.m.calvert@gmail.com>
2025-04-08 20:54:07 -0400

    Initialize lazy config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,25 +8,25 @@ vim.g.mapleader = ","
 -- Load dotenv, if it exists
 require("custom.dotenv").eval(vim.fs.joinpath(vim.fn.stdpath "config", ".env")) ---@diagnostic disable-line: param-type-mismatch
 
--- local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
--- if not vim.uv.fs_stat(lazypath) then
---   vim.fn.system {
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable",
---     lazypath,
---   }
--- end
--- 
--- -- Add lazy to the `runtimepath`, this allows us to `require` it.
--- ---@diagnostic disable-next-line: undefined-field
--- vim.opt.rtp:prepend(lazypath)
--- 
--- -- Set up lazy, and load my `lua/custom/plugins/` folder
--- require("lazy").setup({ import = "custom/plugins" }, {
---   change_detection = {
---     notify = false,
---   },
--- })
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+
+-- Add lazy to the `runtimepath`, this allows us to `require` it.
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up lazy, and load my `lua/custom/plugins/` folder
+require("lazy").setup({ import = "custom/plugins" }, {
+  change_detection = {
+    notify = false,
+  },
+})
