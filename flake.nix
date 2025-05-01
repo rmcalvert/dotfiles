49151e6111379d7bb01380a1f8f244853af44a09
Ryan Calvert <ryan.m.calvert@gmail.com>
2025-05-01 09:06:43 -0400

    Refactor wip

--- flake.nix
+++ flake.nix
@@ -2,163 +2,190 @@
   description = "Nix System Configuration";
 
   inputs = {
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     darwin = {
       url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    # Optional: Declarative tap management
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
+
+    ##! nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    ##! # Optional: Declarative tap management
+    ##! homebrew-core = {
+    ##!   url = "github:homebrew/homebrew-core";
+    ##!   flake = false;
+    ##! };
+    ##! homebrew-cask = {
+    ##!   url = "github:homebrew/homebrew-cask";
+    ##!   flake = false;
+    ##! };
+    ##! homebrew-bundle = {
+    ##!   url = "github:homebrew/homebrew-bundle";
+    ##!   flake = false;
+    ##! };
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  # self,
+  # nixpkgs-unstable,
+  # darwin,
+  # nix-homebrew,
+  # homebrew-core,
+  # homebrew-cask,
+  # homebrew-bundle,
+  # home-manager,
+  # nixos-wsl,
   outputs =
     {
-      self,
       nixpkgs,
-      nixpkgs-unstable,
-      darwin,
-      nix-homebrew,
-      homebrew-core,
-      homebrew-cask,
-      homebrew-bundle,
-      home-manager,
-      nixos-wsl,
       ...
     }@inputs:
     let
-      darwinSystem =
-        {
-          user,
-          system ? "aarch64-darwin",
-        }:
-        # let
-        # pkgs = nixpkgs.legacyPackages.${system};
-        # inherit (nixpkgs.legacyPackages.${system}) lib;
-        # nixpkgsConfig = {
-        #   inherit system;
-        #   config.allowUnfree = true;
-        # };
-
-        # nixpkgs.for = {
-        # Pattern from https://github.com/chris-martin/home/blob/dc79903c93f654108ea3c05cfd779bdef72eb584/os/flake.nix
-        #vscode = import inputs."nixpkgs-for-vscode-${hostname}" nixpkgsConfig;
-        #hoogle = import inputs."nixpkgs-for-hoogle-${hostname}" nixpkgsConfig;
-        # };
-        # nixpkgs.from = {
-        #   stable = import nixpkgs-stable nixpkgsConfig;
-        #   unstable = import nixpkgs-unstable nixpkgsConfig;
-        # };
-
-        # in
-        # unstable = import nixpkgs-unstable { inherit system; };
-        #       unstable = import nixpkgs-unstable {
-        #         system = final.system;
-        #       };
-        darwin.lib.darwinSystem {
-          system = system;
-          modules = [
-            ./darwin/darwin.nix
-            nix-homebrew.darwinModules.nix-homebrew
-            {
-              nix-homebrew = {
-                # Install Homebrew under the default prefix
-                enable = true;
-
-                # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-                enableRosetta = true;
-
-                # User owning the Homebrew prefix
-                user = user;
-
-                # Optional: Declarative tap management
-                taps = {
-                  "homebrew/homebrew-core" = homebrew-core;
-                  "homebrew/homebrew-cask" = homebrew-cask;
-                  "homebrew/homebrew-bundle" = homebrew-bundle;
-                };
-
-                # Optional: Enable fully-declarative tap management
-                #
-                # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-                mutableTaps = false;
-              };
-            }
-            home-manager.darwinModules.home-manager
-            {
-              _module.args = { inherit inputs; };
-              home-manager = {
-                extraSpecialArgs = {
-                  inherit inputs;
-                };
-                users.${user} = import ./home-manager;
-                useGlobalPkgs = true;
-                useUserPackages = true;
-              };
-              users.users.${user} = {
-                home = "/Users/${user}";
-                # shell = pkgs.zsh;
-              };
-              # environment.shells = with pkgs; [ zsh ];
-              # users.defaultUserShell = pkgs.zsh;
-              nix.settings.trusted-users = [ user ];
-            }
-          ];
-        };
+      overlays = [
+        # This overlay makes unstable packages available through pkgs.unstable
+        (final: prev: {
+          unstable = import inputs.nixpkgs-unstable {
+            system = prev.system;
+            config.allowUnfree = true;
+          };
+        })
+      ];
+      mkSystem = import ./lib/mksystem.nix {
+        inherit overlays nixpkgs inputs;
+      };
     in
     {
-      # nixosConfigurations = {
-      #   nixos = nixpkgs.lib.nixosSystem {
-      #     system = "x86_64-linux";
-      #     modules = [
-      #       nixos-wsl.nixosModules.wsl
-      #       ./nixos/configuration.nix
-      #       ./dot_config/wsl
-      #       home-manager.nixosModules.home-manager
-      #       {
-      #         home-manager = {
-      #           users.nixos = import ./home-manager;
-      #           useGlobalPkgs = true;
-      #           useUserPackages = true;
-      #         };
-      #         nix.settings.trusted-users = [ "nixos" ];
-      #       }
-      #     ];
-      #   };
-      # };
-      darwinConfigurations = {
-        "Ryans-MacBook-Pro" = darwinSystem {
-          user = "ryan";
-          system = "aarch64-darwin";
-        };
-        "Ryans-Mac-mini" = darwinSystem {
-          user = "ryan";
-          system = "aarch64-darwin";
-        };
+      nixosConfigurations.wsl = mkSystem "wsl" {
+        system = "x86_64-linux";
+        user = "rmcalvert";
+        wsl = true;
+      };
+      darwinConfigurations.Ryans-MacBook-Pro = mkSystem "mac" {
+        system = "aarch64-darwin";
+        user = "ryan";
+        darwin = true;
+      };
+      darwinConfigurations.Ryans-Mac-mini = mkSystem "mac" {
+        system = "aarch64-darwin";
+        user = "ryan";
+        darwin = true;
       };
     };
+  ##!     let
+  ##!       darwinSystem =
+  ##!         {
+  ##!           user,
+  ##!           system ? "aarch64-darwin",
+  ##!         }:
+  ##!         # let
+  ##!         # pkgs = nixpkgs.legacyPackages.${system};
+  ##!         # inherit (nixpkgs.legacyPackages.${system}) lib;
+  ##!         # nixpkgsConfig = {
+  ##!         #   inherit system;
+  ##!         #   config.allowUnfree = true;
+  ##!         # };
+  ##!
+  ##!         # nixpkgs.for = {
+  ##!         # Pattern from https://github.com/chris-martin/home/blob/dc79903c93f654108ea3c05cfd779bdef72eb584/os/flake.nix
+  ##!         #vscode = import inputs."nixpkgs-for-vscode-${hostname}" nixpkgsConfig;
+  ##!         #hoogle = import inputs."nixpkgs-for-hoogle-${hostname}" nixpkgsConfig;
+  ##!         # };
+  ##!         # nixpkgs.from = {
+  ##!         #   stable = import nixpkgs-stable nixpkgsConfig;
+  ##!         #   unstable = import nixpkgs-unstable nixpkgsConfig;
+  ##!         # };
+  ##!
+  ##!         # in
+  ##!         # unstable = import nixpkgs-unstable { inherit system; };
+  ##!         #       unstable = import nixpkgs-unstable {
+  ##!         #         system = final.system;
+  ##!         #       };
+  ##!         darwin.lib.darwinSystem {
+  ##!           system = system;
+  ##!           modules = [
+  ##!             ./darwin/darwin.nix
+  ##!             nix-homebrew.darwinModules.nix-homebrew
+  ##!             {
+  ##!               nix-homebrew = {
+  ##!                 # Install Homebrew under the default prefix
+  ##!                 enable = true;
+  ##!
+  ##!                 # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+  ##!                 enableRosetta = true;
+  ##!
+  ##!                 # User owning the Homebrew prefix
+  ##!                 user = user;
+  ##!
+  ##!                 # Optional: Declarative tap management
+  ##!                 taps = {
+  ##!                   "homebrew/homebrew-core" = homebrew-core;
+  ##!                   "homebrew/homebrew-cask" = homebrew-cask;
+  ##!                   "homebrew/homebrew-bundle" = homebrew-bundle;
+  ##!                 };
+  ##!
+  ##!                 # Optional: Enable fully-declarative tap management
+  ##!                 #
+  ##!                 # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+  ##!                 mutableTaps = false;
+  ##!               };
+  ##!             }
+  ##!             home-manager.darwinModules.home-manager
+  ##!             {
+  ##!               _module.args = { inherit inputs; };
+  ##!               home-manager = {
+  ##!                 extraSpecialArgs = {
+  ##!                   inherit inputs;
+  ##!                 };
+  ##!                 users.${user} = import ./home-manager;
+  ##!                 useGlobalPkgs = true;
+  ##!                 useUserPackages = true;
+  ##!               };
+  ##!               users.users.${user} = {
+  ##!                 home = "/Users/${user}";
+  ##!                 # shell = pkgs.zsh;
+  ##!               };
+  ##!               # environment.shells = with pkgs; [ zsh ];
+  ##!               # users.defaultUserShell = pkgs.zsh;
+  ##!               nix.settings.trusted-users = [ user ];
+  ##!             }
+  ##!           ];
+  ##!         };
+  ##!     in
+  ##!     {
+  ##!       # nixosConfigurations = {
+  ##!       #   nixos = nixpkgs.lib.nixosSystem {
+  ##!       #     system = "x86_64-linux";
+  ##!       #     modules = [
+  ##!       #       nixos-wsl.nixosModules.wsl
+  ##!       #       ./nixos/configuration.nix
+  ##!       #       ./dot_config/wsl
+  ##!       #       home-manager.nixosModules.home-manager
+  ##!       #       {
+  ##!       #         home-manager = {
+  ##!       #           users.nixos = import ./home-manager;
+  ##!       #           useGlobalPkgs = true;
+  ##!       #           useUserPackages = true;
+  ##!       #         };
+  ##!       #         nix.settings.trusted-users = [ "nixos" ];
+  ##!       #       }
+  ##!       #     ];
+  ##!       #   };
+  ##!       # };
+  ##!     };
 }
--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,67 +1,9 @@
 {
-  pkgs,
-  lib,
-  inputs,
-  ...
-}:
-{
-  imports = [
-    inputs.nixvim.homeManagerModules.nixvim
-    ./nixvim
-    ./git.nix
-    ./starship.nix
-    ./wezterm.nix
-    #   ./vscode.nix
-    # ./tmux.nix
-  ];
-
-  xdg.enable = true;
-  xdg.configFile."hammerspoon" = lib.mkIf pkgs.stdenv.isDarwin {
-    source = ./../dot_config/hammerspoon;
-  };
-  xdg.configFile."kanata" = lib.mkIf pkgs.stdenv.isDarwin { source = ./../dot_config/kanata; };
-  # xdg.configFile."ghostty/config".source = ./../dot_config/ghostty/config;
 
   fonts.fontconfig.enable = true;
 
   home = {
     stateVersion = "23.05"; # Please read the comment before changing.
-
-    # The home.packages option allows you to install Nix packages into your
-    # environment.
-    packages = with pkgs; [
-      amber
-      lazygit
-      markdown-oxide
-      nixd
-      nixfmt-rfc-style
-      just # https://github.com/casey/just
-      nix-search-cli
-      ripgrep
-      smartcat
-
-      # Fonts
-      fira-code
-      atkinson-hyperlegible
-      jetbrains-mono
-      cascadia-code
-    ];
-
-    sessionVariables = { };
   };
 
-  home.file.".gemrc".source = ../dot_config/.gemrc;
-
-  programs = {
-    zsh = {
-      enable = true;
-    };
-
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-
-    # jujutsu.enable = true;
-  };
 }
